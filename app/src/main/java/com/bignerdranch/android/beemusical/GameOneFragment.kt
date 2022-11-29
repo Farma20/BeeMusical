eb9cf12ae3ee9db6119889c880b9b588cc613bdb
ÐFarma20 <kustov.ius@students.dvfu.ru>
2022-11-29 23:57:02 +1000

    Изменена реализация tts. Теперь tts инкапсулирован в OneGameFragment вместо MainActivity

--- app/src/main/java/com/bignerdranch/android/beemusical/GameOneFragment.kt
+++ app/src/main/java/com/bignerdranch/android/beemusical/GameOneFragment.kt
@@ -2,6 +2,9 @@ package com.bignerdranch.android.beemusical
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
 import android.view.LayoutInflater
 import android.view.View
@@ -14,17 +17,22 @@ import java.util.*
 
 class GameOneFragment: Fragment() {
     private lateinit var hostActivity: MainActivity
+
+    //Создание ппеременной, хранящей класс с генератором озвучивания текста
+    lateinit var mTTs: TextToSpeech
+
     private lateinit var gameBackgroundImageView: ImageView
-    private lateinit var DO: ImageView
-    private lateinit var RE: ImageView
-    private lateinit var MI: ImageView
-    private lateinit var FA: ImageView
-    private lateinit var SOL: ImageView
-    private lateinit var LA: ImageView
-    private lateinit var SI: ImageView
-    private lateinit var DO_last: ImageView
+    private lateinit var noteDo: ImageView
+    private lateinit var noteRe: ImageView
+    private lateinit var noteMi: ImageView
+    private lateinit var noteFa: ImageView
+    private lateinit var noteSol: ImageView
+    private lateinit var noteLa: ImageView
+    private lateinit var noteSi: ImageView
+    private lateinit var noteDoLast: ImageView
     private lateinit var listImageView: List<ImageView>
     private lateinit var congras:ImageView
+
 
 
     override fun onCreateView(
@@ -35,28 +43,48 @@ class GameOneFragment: Fragment() {
         val view = inflater.inflate(R.layout.fragment_game_one, container, false)
 
         gameBackgroundImageView = view.findViewById(R.id.background)
-        DO = view.findViewById(R.id.one_game_DO)
-        RE = view.findViewById(R.id.one_game_RE)
-        MI = view.findViewById(R.id.one_game_MI)
-        FA = view.findViewById(R.id.one_game_FA)
-        SOL = view.findViewById(R.id.one_game_SOL)
-        LA = view.findViewById(R.id.one_game_LA)
-        SI = view.findViewById(R.id.one_game_SI)
-        DO_last = view.findViewById(R.id.one_game_DO_last)
+        noteDo = view.findViewById(R.id.one_game_DO)
+        noteRe = view.findViewById(R.id.one_game_RE)
+        noteMi = view.findViewById(R.id.one_game_MI)
+        noteFa = view.findViewById(R.id.one_game_FA)
+        noteSol = view.findViewById(R.id.one_game_SOL)
+        noteLa = view.findViewById(R.id.one_game_LA)
+        noteSi = view.findViewById(R.id.one_game_SI)
+        noteDoLast = view.findViewById(R.id.one_game_DO_last)
 
-        listImageView = listOf(DO, RE, MI, FA, SOL, LA, SI, DO_last)
+        listImageView = listOf(noteDo, noteRe, noteMi, noteFa, noteSol, noteLa, noteSi, noteDoLast)
 
         congras = view.findViewById(R.id.congras)
 
+        //Инициализация tts через контекст и onInit ассинфхронную функцию
+        mTTs = TextToSpeech(this.context) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                val locale = Locale("RU")
+                mTTs.language = locale
+                speak("Стоит на поле домик. Рядом растет репка. Вечером приходит мишка, " +
+                        "зажигает факел и ложится спать. А утром выглядывает солнышко. Квакают ля гушки." +
+                        " и синички летят в свой маленький домик")
+            }
+        }
 
         return view
+    }
+
+    //функция активирующая произношение слов
+    private fun speak(str:String){
+        val text: String = str
+        val map = HashMap<String, String>()
+        map[TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID] = "UniqueID"
+        mTTs.speak(text, TextToSpeech.QUEUE_FLUSH, map)
     }
 
 
 
     override fun onStart() {
         super.onStart()
-        congras.visibility = View.INVISIBLE
+
+        //Инициалиация объектов взаимодействия с View
+
         val notes: List<String> = listOf(
             "ДОМ",
             "РЕПКА",
@@ -68,49 +96,44 @@ class GameOneFragment: Fragment() {
             "ДОМ"
         )
 
-        val res_orig: List<Int> = listOf(R.drawable.one_game_do, R.drawable.one_game_re,
+        val viewOriginal: List<Int> = listOf(R.drawable.one_game_do, R.drawable.one_game_re,
             R.drawable.one_game_mi, R.drawable.one_game_fa, R.drawable.one_game_sol,
             R.drawable.one_game_la, R.drawable.one_game_si, R.drawable.one_game_do)
 
-        val res_sel: List<Int> = listOf(R.drawable.one_game_do_select, R.drawable.one_game_re_select,
+        val viewSelect: List<Int> = listOf(R.drawable.one_game_do_select, R.drawable.one_game_re_select,
             R.drawable.one_game_mi_select, R.drawable.one_game_fa_select,
             R.drawable.one_game_sol_select, R.drawable.one_game_la_select,
             R.drawable.one_game_si_select, R.drawable.one_game_do_select)
 
+
         var j = 0
 
-        val speechListener1 = object : UtteranceProgressListener(){
+        //Слушатель tts, который отслеживает начало и завершение произношения
+        val speechListener = object : UtteranceProgressListener(){
             override fun onStart(utteranceId: String?) {
-                listImageView[j].setImageResource(res_sel[j])
+                listImageView[j].setImageResource(viewSelect[j])
                 j++
             }
 
             override fun onDone(utteranceId: String?) {
                 if(j == listImageView.size){
-                    listImageView[j-1].setImageResource(res_orig[j-1])
+                    listImageView[j-1].setImageResource(viewOriginal[j-1])
+                    return
 //
                 }else{
-                    hostActivity.speak("Скажи ${notes[j]}")
+                    speak("Скажи ${notes[j]}")
                     gameBackgroundImageView.setImageResource(R.drawable.one_game_background_lourge)
-                    listImageView[j-1].setImageResource(res_orig[j-1])
+                    listImageView[j-1].setImageResource(viewOriginal[j-1])
                 }
             }
 
+            @Deprecated("Deprecated in Java", ReplaceWith("println(\"TTS ERROR\")"))
             override fun onError(utteranceId: String?) {
-
+                println("TTS ERROR")
             }
         }
 
-
-
-        hostActivity.mTTs.setOnUtteranceProgressListener(speechListener1)
-
-
-        hostActivity.speak("Стоит на поле домик.Рядом растет репка. Вечером приходит мишка" +
-                "зажигает факел и ложится спать. А утром выглядывает солнышко. Квакают ля гушки." +
-                "и синички летят в свой маленький домик")
-
-
+        mTTs.setOnUtteranceProgressListener(speechListener)
 
 
     }
@@ -125,6 +148,8 @@ class GameOneFragment: Fragment() {
 
     override fun onDetach() {
         super.onDetach()
+        mTTs.stop()
+        mTTs.shutdown()
 
     }
 
--- app/src/main/java/com/bignerdranch/android/beemusical/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/beemusical/MainActivity.kt
@@ -8,21 +8,13 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mTTs: TextToSpeech
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Убираем верхнюю панель с названием
         supportActionBar?.hide()
 
-        mTTs = TextToSpeech(this) { status ->
-            if (status == TextToSpeech.SUCCESS) {
-                val locale: Locale = Locale("RU")
-                val result = mTTs.setLanguage(locale)
-            }
-
-        }
 
         //Транзакция фрагмента
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
@@ -34,13 +26,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun speak(text_:String){
-        val text: String = text_
-
-        val map = HashMap<String, String>()
-        map[TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID] = "UniqueID";
-        mTTs.speak(text, TextToSpeech.QUEUE_FLUSH, map)
-    }
 
     fun onFragmentSelected(fragment: Fragment){
         supportFragmentManager.beginTransaction()
@@ -48,14 +33,5 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(null)
             .commit()
     }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        if(mTTs != null){
-            mTTs.stop()
-            mTTs.shutdown()
-        }
-    }
-
 
 }
